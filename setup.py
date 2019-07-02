ab6d9cf6d62c23fdc0cf07cd897600ac4e84dc80
Eivind Bøhn <eivind.bohn@gmail.com>
2019-07-02 14:56:36 +0200

    change setup back to pyfly

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "python-dateutil==2.8.0",
         "scipy==1.3.0",
         "six==1.12.0",
-        "pyflight==0.1.0"
+        "pyfly==0.1.0"
     ],
-    dependency_links=['http://github.com/eivindeb/pyfly/tarball/master#egg=pyflight-0.1.0']
+    dependency_links=['http://github.com/eivindeb/pyfly/tarball/master#egg=pyfly-0.1.0']
 )
