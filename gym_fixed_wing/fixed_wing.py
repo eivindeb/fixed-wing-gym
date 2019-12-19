0299d7ed1625eaedaade637e97830dad547e2d7e
Eivind Bøhn <eivindeb@github.com>
2019-12-19 14:09:44 +0100

    fix bug with low and high values both outside delta on the same side

--- gym_fixed_wing/fixed_wing.py
+++ gym_fixed_wing/fixed_wing.py
@@ -225,7 +225,7 @@ class FixedWingAircraft(gym.Env):
         Set the curriculum level of the environment, e.g. for starting with simple tasks and progressing to more difficult
         scenarios as the agent becomes increasingly proficient.
 
-        :param level: (int) the curriculum level
+        :param level: (int) the curriculum level between 0 and 1.
         """
         assert 0 <= level <= 1
         self._curriculum_level = level
@@ -516,7 +516,7 @@ class FixedWingAircraft(gym.Env):
             if delta is not None:
                 var_val = self.simulator.state[target_var_name].value
                 low = max(low, var_val - delta)
-                high = min(high, var_val + delta)
+                high = max(min(high, var_val + delta), low)
 
             if self.sampler is None:
                 initial_value = self.np_random.uniform(low, high)
